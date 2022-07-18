4755efef020c2befad07c15f2749b2ebb113db46
qd-qd <qdqdqdqdqd@protonmail.com>
2022-07-18 18:35:00 +0200

    chore: update smart-contract informations

--- src/utils/EIP712.ts
+++ src/utils/EIP712.ts
@@ -1,7 +1,7 @@
 export const domain = {
   name: "ChatBox",
   version: "1",
-  verifyingContract: "0x3B3818C2b8B29c3395589DE3a91bFb391ce5698B",
+  verifyingContract: "0xd156b7CcEE0951eACfFC3f2dbc5Ef7Be3782eA88",
   chainId: 5,
 };
 
