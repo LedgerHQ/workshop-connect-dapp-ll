9f190ede6ff9e48d65b9a310834623aa79a9c488
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-07-18 09:10:18 +0200

    Add optional chaining to messages

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,12 @@
+import { Flex } from "@ledgerhq/react-ui";
 import ChatBox from "../components/ChatBox";
 import MessageCard from "../components/MessageCard";
-import { Flex } from "@ledgerhq/react-ui";
 import { Message } from "../utils/types";
 
 const Home = ({ messages }: { messages: Array<Message> }) => (
   <main style={{ maxWidth: "40rem", paddingTop: "3rem" }}>
     <ChatBox />
-    {messages.length ? (
+    {messages?.length ? (
       <Flex flexDirection="column" rowGap="1rem" mt="2rem">
         {messages?.map((message) => (
           <MessageCard key={message.id} message={message} />
