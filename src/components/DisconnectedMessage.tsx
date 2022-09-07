2c01eeb0a44b98952aa49bde38e11021c3d8a228
qd-qd <qdqdqdqdqd@protonmail.com>
2022-09-07 14:59:04 +0200

    ✨ add disconnected message
    this message will be displayed to the user if no account is connected

---
+++ src/components/DisconnectedMessage.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "@ledgerhq/react-ui";
+
+const DisconnectedMEssage = () => (
+  <Flex>
+    <Text variant="body">⚠️ You must be logged in to access the dApp</Text>
+  </Flex>
+);
+
+export default DisconnectedMEssage;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,19 +1,27 @@
 import { Flex } from "@ledgerhq/react-ui";
 import ChatBox from "../components/ChatBox";
 import MessageCard from "../components/MessageCard";
+import DisconnectedMessage from "../components/DisconnectedMessage";
 import useFetchMessages from "../api/useFetchMessages";
 import { ethers } from "ethers";
+import { useAccount } from "wagmi";
 
 const Home = () => {
   const { data: messages } = useFetchMessages();
-  const filteredMessages = messages.filter(
+  const { isConnected } = useAccount();
+
+  // don't render the app if the user isn't connected with an EOA
+  if (!isConnected) return <DisconnectedMessage />;
+
+  // filter empty messages
+  const filteredMessages = messages?.filter(
     (message) => message.author !== ethers.constants.AddressZero
   );
 
   return (
     <main style={{ maxWidth: "40rem", paddingTop: "3rem" }}>
       <ChatBox />
-      {filteredMessages.length ? (
+      {filteredMessages?.length ? (
         <Flex flexDirection="column" rowGap="1rem" mt="2rem">
           {filteredMessages.map((message) => (
             <MessageCard key={message.id.toString()} message={message} />
