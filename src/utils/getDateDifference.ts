9dcf93322a83e0590e3cab07a34e8215ce8afb33
qd-qd <qdqdqdqdqd@protonmail.com>
2022-09-08 21:53:26 +0200

    🐛 fix message timestamp

--- src/utils/getDateDifference.ts
+++ src/utils/getDateDifference.ts
@@ -1,16 +1,27 @@
-const rtf = new Intl.RelativeTimeFormat("en", {
-  localeMatcher: "best fit",
-  numeric: "always",
+const formatter = new Intl.RelativeTimeFormat("en", {
+  numeric: 'always',
   style: "long",
 });
 
-// Return the difference in hours or days
-function getDateDifference(fromDate, toDate) {
-  let diff = Math.floor((fromDate - toDate) / (1000 * 60 * 60 * 24));
-  if (diff > 1) return rtf.format(diff, "day");
+const DIVISIONS: Array<{ amount: number, name: Intl.RelativeTimeFormatUnit }> = [
+  { amount: 60, name: 'seconds' },
+  { amount: 60, name: 'minutes' },
+  { amount: 24, name: 'hours' },
+  { amount: 7, name: 'days' },
+  { amount: 4.34524, name: 'weeks' },
+  { amount: 12, name: 'months' },
+  { amount: Number.POSITIVE_INFINITY, name: 'years' }
+];
 
-  diff = Math.floor((fromDate - toDate) / (1000 * 60 * 60));
-  return rtf.format(diff, "hours");
+const getDateDifference = (date, now) => {
+  let duration = (date - now) / 1000
+
+  for (let i = 0; i <= DIVISIONS.length; i++) {
+    const division = DIVISIONS[i]
+    if (Math.abs(duration) < division.amount)
+      return formatter.format(Math.round(duration), division.name)
+    duration /= division.amount
+  }
 }
 
 export default getDateDifference;
