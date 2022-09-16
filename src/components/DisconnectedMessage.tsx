ddf837bd1d5df145edcbd6dc6c8847cefaaef73c
fcipollone-ledger <74672573+fcipollone-ledger@users.noreply.github.com>
2022-09-16 12:16:16 +0200

    Update DisconnectedMessage.tsx

--- src/components/DisconnectedMessage.tsx
+++ src/components/DisconnectedMessage.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from "@ledgerhq/react-ui";
 
 const DisconnectedMEssage = () => (
   <Flex>
-    <Text variant="body">⚠️ You must be logged in to access the dApp</Text>
+    <Text variant="body">⚠️ You must be logged in to run the DApp</Text>
   </Flex>
 );
 
