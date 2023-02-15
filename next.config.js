0b09a8daa64952cfc41a3420f56193907669ea7c
qd-qd <qdqdqdqdqd@protonmail.com>
2023-02-15 11:01:46 +0100

    📌 block deps update to minor versions
    Use the tilde character to block updates to minor versions instead of the
    caret character. This workshop and its Ledger dependencies will not be maintained
    anymore, so we don't want to risk breaking changes.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   reactStrictMode: true,
-  experimental: {
-    output: "standalone",
-  },
 };
 
 module.exports = nextConfig;
